0822005aad6437f7b4ce4cc9552bb1d555309487
Gabriele <susta4e@hotmail.it>
2023-05-01 16:53:08 +0200

    Algoritmo JS

--- Plugin.js
+++ Plugin.js
@@ -4,26 +4,37 @@ toggle between hiding and showing the dropdown content */
 
 
 const cromaticScale = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-const keyboard = [
+/*const keyboard = [
 ["E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C3", "C#3", "D3", "D#3"],
 ["B2", "C2", "C#2", "D2", "D#2", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3"],
 ["G2", "G#2", "A2", "A#2", "B2", "C2", "C#2", "D2", "D#2", "E3", "F3", "F#3"],
 ["D1", "D#1", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2", "C2", "C#2"],
 ["A1", "A#1", "B1", "C1", "C#1", "D1", "D#1", "E2", "F2", "F#2", "G2", "G#2"],
 ["E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1", "C1", "C#1", "D1", "D#1"]
-]; 
+]; */
 
-const majRule = [4, 7];
+
+
+  const keyboard = [
+    ["E3", "F3", "F#3", "G3", "G#3"],
+    ["B2", "C2", "C#2", "D2", "D#2"],
+    ["G2", "G#2", "A2", "A#2", "B2"],
+    ["D1", "D#1", "E2", "F2", "F#2"],
+    ["A1", "A#1", "B1", "C1", "C#1"],
+    ["E1", "F1", "F#1", "G1", "G#1"]
+    ]; 
+
+const majRule = [4, 7]; 
 const minRule = [3, 7];
 const dimRule = [3, 6];
 const augRule = [5, 8];
+const rule7   = 13;
+const rule9   = 14;
 let chordPieces = ["", "", ""];
 let chord ="";
 let progressionPieces = [];
 let chordProgression = "";
 let chordProgressionLength = 4;
-let numCicli = 10;
-let computedChord = ["", "", "", ""];
 
 // Close the dropdown menu that is already open and opens the clicked one
 function exitFunction(num) {
@@ -109,7 +120,7 @@ function RemoveChord(){
        chordProgression = chordProgression.toString() + " - ";
       }
     }
-    //console.log(progressionPieces);
+    //echo(progressionPieces);
    document.getElementById("chordProgression").innerHTML = chordProgression;
   }
 }
@@ -122,25 +133,103 @@ function ClearChords(){
 ////////////////////////////////// TAB GENERATION SECTION ////////////////////////////////
 
 
+
+
+
 function GenerateTab(){
-  //Loop for the iterations of the computation algoithm 
-  for(var ciclo = 0; ciclo < numCicli; ciclo ++){
-    //Loop on the chord sequence
-    for(var i = 0; i<progressionPieces.length; i++){
-      //Condition valid only for the first computation of the first chord
-      if(i == 0 && ciclo == 0){GenerateChord(ciclo = ciclo, chord1 = progressionPieces[i] )}
-      //GenerateChord(chord1 = progressionPieces[i], chord0 = progressionPieces[i-1], ciclo = ciclo);
+let matches;
+let voicing;
+let output = [];
+  echo(progressionPieces);
+
+  //Loop on the chord sequence
+  for(var i = 0; i<progressionPieces.length; i++){
+    voicing = [];
+    let consideredChord = [];
+    matches = [];
+    //Generating chord using rules and chromatic scale
+    consideredChord = GenerateChord(inputChord = progressionPieces[i]);
+    echo(`Considered Chord: ${consideredChord}`);
+
+    //For every note of the considered chord
+    for(var h=0; h<consideredChord.length; h++){
+
+      echo(`Nota considerata: ${consideredChord[h]}`);
+
+      //For every string of the guitar collects all the matches
+      for(var k=0; k<keyboard.length; k++){
+        //echo(`String considerata: ${keyboard[k]}`);
+        if(keyboard[k].some(e => e.includes(consideredChord[h]))){
+          //echo("Match: ");
+          //echo(keyboard[k].findIndex(element => element.includes(consideredChord[h].toString())));
+          matches.push({'note': consideredChord[h], 'string': k, 'fret': keyboard[k].findIndex(element => element.includes(consideredChord[h].toString()))});
+        }
+      }
     }
+
+
+
+  echo(`Matches: ${JSON.stringify(matches)}`);
+
+    while(voicing.length<consideredChord.length-1){
+      let left = null;
+      let right = null;
+      voicing = [];
+      //For every note
+      for(let m=0; m<consideredChord.length; m++){
+        //For every match found for that note
+        for(let n=0; n<matches.length; n++){
+          if(matches[n].note == consideredChord[m]){
+            if(left != null && matches[n].string == left -1){
+              left = matches[n].string;
+              voicing.push(keyboard[matches[n].string][matches[n].fret]);
+              n = matches.length;
+            }else{
+              if(right != null && matches[n].string == right +1){
+              right = matches[n].string;
+              voicing.push(keyboard[matches[n].string][matches[n].fret]);
+              n = matches.length;
+              }else{
+                if(left == null && left == null){
+                left = matches[n].string;
+                right = matches[n].string;
+                voicing.push(keyboard[matches[n].string][matches[n].fret]);
+                matches.splice(n, 1);
+                n = matches.length;
+                }
+              }
+            }
+            
+            
+
+          }
+        }
+      }
+    }
+    echo(`output parziale:${voicing}`);
+    output.push(voicing);
+  }
+  echo(`output: ${JSON.stringify(output)}`);
+  return(output);
+}
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
   }
 }
 
-function GenerateChord(ciclo, chord1, chord0){
-  // Initial chord generation
-  if(chord0 == undefined){
-    //Select the starting note on the cromatic scale
-    start = cromaticScale. indexOf(chord1[0]);
+
+
+function GenerateChord(inputChord){
+  let computedChord = []
+  //Select the starting note on the cromatic scale
+    start = cromaticScale. indexOf(inputChord[0]);
     //Select the mode of the chord
-    switch(chord1[1].toString()){
+    switch(inputChord[1].toString()){
       case "Maj": 
       chosenRule = majRule;
       break;
@@ -154,20 +243,22 @@ function GenerateChord(ciclo, chord1, chord0){
       chosenRule = augRule;
       break;
     }
-    computedChord[0] = chord1[0];
-    computedChord[1] = cromaticScale[start + chosenRule[0]];
-    computedChord[2] = cromaticScale[start + chosenRule[1]];
+    computedChord.push(inputChord[0], cromaticScale[start + chosenRule[0]],  cromaticScale[start + chosenRule[1]]);
+    
     //If present, computes the variation of the chord
-    switch(chord1[2]){
-      case '7': 
-      computedChord[3] = cromaticScale[parseInt(chord1[2].toString()) + start + 4];
-      break;
-      case '9':
-      computedChord[3] = cromaticScale[parseInt(chord1[2].toString()) + start + 5];
-      break;
+    if(inputChord[2] != "" ){
+      switch(inputChord[2].toString()){
+        case '7': 
+        computedChord[3] = cromaticScale[start + rule7];
+        break;
+        case '9':
+        computedChord[3] = cromaticScale[start + rule9];
+        break;
+      }
     }
     return(computedChord);
-  } else {
-    
-  }
+}
+
+function echo(toPrint){
+  console.log(toPrint);
 }
